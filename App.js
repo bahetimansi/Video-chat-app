5dfdb5ccd363c503a22478f8447f85e85a486704
bahetimansi <mansibaheti2603@gmail.com>
2022-07-06 12:29:56 +0530

    hehhehehe

--- App.js
+++ App.js
@@ -1,12 +1,20 @@
 import React from "react";
 import { Typography, AppBar } from "@material-ui/core";
 
+
+import VideoPlayer from "./Video-chat-app/client/src/components/VideoPlayer";
+import Notifications from "./Video-chat-app/client/src/components/Notifications";
+import Options from "./Video-chat-app/client/src/components/Options";
 const App = ()  => {
     return(
         <div>
             < AppBar position="static" color="inherit">
                 <Typography variant="h2" align="center">Video Chat</Typography>
             </AppBar>
+            < VideoPlayer />
+            <Options>
+                <Notifications />
+                </Options> 
         </div>
     );
 }
